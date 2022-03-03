ab188277af57f8ba98ad0a295263422009293f60
Tienluc037 <nguyentienluc12a3@gmail.com>
2022-03-03 09:26:27 +0700

    update blog

---
+++ MD3-Eloquent-ORM/app-blog/app/Http/Controllers/BlogController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        $blogs = Blog::all();
+        return view('blog.list',compact('blogs'));
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
+}
